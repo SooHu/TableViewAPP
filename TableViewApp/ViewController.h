b02cfc3e73b9bf98bfd455090112d5344ae2e3cd
HuS <zjhs9527@163.com>
2015-06-04 15:21:18 +0800

    Initial Commit

---
+++ TableViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewApp
+//
+//  Created by HuS on 15/6/4.
+//  Copyright (c) 2015年 HuS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewApp
+//
+//  Created by HuS on 15/6/4.
+//  Copyright (c) 2015年 HuS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
